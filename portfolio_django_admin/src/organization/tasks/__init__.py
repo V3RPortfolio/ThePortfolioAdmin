28c70235e2c8fa1c88e208278840de89c3c50c51
Zuhair Mehtab <zuhairmhtb@gmail.com>
2026-05-20 11:27:32 +0600

    Export update_device_heartbeat task from tasks package

--- portfolio_django_admin/src/organization/tasks/__init__.py
+++ portfolio_django_admin/src/organization/tasks/__init__.py
@@ -1,3 +1,4 @@
 from .process_invitation import process_invitation
+from .update_device_heartbeat import update_device_heartbeat
 
-__all__ = ["process_invitation"]
+__all__ = ["process_invitation", "update_device_heartbeat"]
